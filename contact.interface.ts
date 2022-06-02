e344c60e6ca4923c3b4b11ca4fa1bbe218f2e8e8
diaby0 <97156358+diaby0@users.noreply.github.com>
2022-06-02 08:11:20 +0200

    Add files via upload

---
+++ contact.interface.ts
@@ -0,0 +1,10 @@
+     export interface contact {
+        nom: string,
+        prenom: string,
+        entre:  string,
+        tel:  string,
+        adresse:  string,
+        mail:  string,
+        note:  string,
+        photo: string
+    }
